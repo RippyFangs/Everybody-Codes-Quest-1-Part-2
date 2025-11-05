1da3f8b5d6f09266e214da07c7d6251c2b64156a
RippyFangs <rippyfangs@gmail.com>
2025-11-05 20:40:09 +0000

    feature: file being read

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::fs;
-use std::io::prelude::*;
+use std::fs::read_to_string;
+//use std::io::prelude::*;
 
 fn main() {
     //ingest data
+
     let filename = "assets/everybody_codes_e2025_q01_p1.txt";
-    let raw_data = file
+
+    let raw_data = match read_to_string(filename) {
+        Ok(data) => data,
+        Err(error) => panic!("Problem opening and reading file: {error:?}"),
+    };
+
+    print!("{}", raw_data);
 
     //structure data
 
@@ -13,4 +20,4 @@ fn main() {
     //print result
 }
 
-fn structure() {}
+//fn structure() {}
